0eab5bf3ac9fd64d15a625cd4c60b377c2738e7b
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-04-21 16:54:53 -0400

    Done.

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ class Circle {
   get diameter() {
     return this.radius * 2;
   }
-
-  get circumference() {
-    return 2 * Math.PI * this.radius;
-  }
-
-  get area() {
-    return this.radius ** 2 * Math.PI;
-  }
-
+  
   // calculate radius based on input value and 
     // set this.radius accordingly
   set diameter(diameter) {
     this.radius = diameter / 2;
   }
 
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+
   set circumference(circumference){
     this.radius = circumference / 2 / Math.PI;
+  }
+
+  get area() {
+    return this.radius ** 2 * Math.PI;
   }
 
   set area(area){
